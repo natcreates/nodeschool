b10dd840ca237e93a4dcb15ea8c28b9fe9e754b7
Dixon <ndixon@LT5155.local>
2017-09-12 13:04:56 +0100

    continued learnyoureact and stream-adventure workshops

--- learnyoureact/views/index.jsx
+++ learnyoureact/views/index.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 export default class TodoBox extends Component {
       render() {
+        // note that components must return a single root element
+        // hence the div wrapping the heading and todo elements
         return (
                 <div className="todoBox">
                     <h1>Todos</h1>
@@ -13,26 +15,47 @@ export default class TodoBox extends Component {
 }
 
 class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { checked: false }
+  }
+
+  handleChange() {
+    this.setState((prevState, props) => {
+      if (prevState.checked === false) {
+        return { checked: true };
+      } else {
+        return { checked: false };
+      }
+    })
+  }
   render() {
     return (
       <tr>
-        <td style={{border: "1px solid black"}}>{this.props.title}</td>
-        <td style={{border: "1px solid black"}}>{this.props.children}</td>
+        <td style={style.tableContent}>
+                        <input type="checkbox"
+                          checked={this.state.checked} onChange={this.handleChange.bind(this)}/></td>
+        <td style={style.tableContent}>{this.props.title}</td>
+        <td style={style.tableContent}>{this.props.children}</td>
       </tr>
     )
   }
 }
+Todo.propTypes = {
+      title: React.PropTypes.string.isRequired
+};
 
 class TodoList extends Component {
   render() {
     return (
-      <div className="todoList">
+    <div className="todoList">
         <table style={{border: "2px solid black"}}>
             <tbody>
-            <Todo title="Shopping">Milk</Todo>
-            <Todo title="Hair cut">13:00</Todo>
+              <Todo title="Shopping">Milk</Todo>
+              <Todo title="Hair cut">13:00</Todo>
+              <Todo title="Learn React">15:00</Todo>
             </tbody>
-        </table>
+          </table>
     </div>
 
     )
@@ -44,3 +67,9 @@ class TodoForm extends Component {
     return <div className="todoForm">I am a TodoForm.</div>
   }
 }
+
+let style = {
+  tableContent: {
+      border: "1px solid black"
+  }
+};
--- stream-adventure/12-dup-redux.js
+++ stream-adventure/12-dup-redux.js
@@ -1,27 +1,48 @@
 const duplexer = require('duplexer2'),
 	through = require('through2');
 
-
 module.exports = function(counter) {
-	var writable = through(write, end);
-	counter.pipe(writable);
 	var countries = {};
+	var writable = through(write, end);
 
-	function write (line, encoding, next) {
-
-		if (countries[line.country]) {
-			countries[line.country]++;
+	function write(data, enc, next) {
+		if (countries[chunk.country]) {
+			countries[chunk.country]++;
 		} else {
-			countries[line.country] = 1;
+			countries[chunk.country] = 1;
 		}
-	// then proceed
-	next();
-	}
+		next();
+  }
 
-	function end (done) {
-		console.log(countries);
+  function end() {
 		counter.setCounts(countries);
-		return duplexer(writable, counter);
 		done();
-	}
+  }
+
+	return duplexer({objectMode: true}, writable, counter);
 }
+
+// module.exports = function(counter) {
+// 	let writable = through(write, end);
+// 	counter.pipe(writable);
+// 	let countries = {};
+//
+// 	function write (line, encoding, next) {
+//
+// 		if (countries[line.country]) {
+// 			countries[line.country]++;
+// 		} else {
+// 			countries[line.country] = 1;
+// 		}
+// 	// then proceed
+// 	console.log(countries);
+// 	next();
+// 	}
+//
+// 	function end (done) {
+// 		console.log(countries);
+// 		counter.setCounts(countries);
+// 		return duplexer(writable, counter);
+// 		done();
+// 	}
+// }
